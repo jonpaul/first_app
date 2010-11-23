b41daee714041a2d0eacc35fa00c7503bde00a1a
Jon-Paul Lussier <jp@gamestreamer.net>
2010-11-23 13:37:56 -0500

    Initital-2010-11-23

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6ea7d3c4938ec7d5ae946c24d2b8398107874f4bba1151122a14975d09f31f1ee514264915a6013e22d2ce5d409c342f41fac3ad1a0e804ba0ac4167d45493f2'
